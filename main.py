b0438df83e8af4713a08c18aaa293b3579823086
Feranmi <orekoyaferanmi5@gmail.com>
2024-06-03 18:40:16 +0100

    data ingestion added

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 from cnnclassifier import logger 
-logger.info("Welcome to cnnclassifier")
+from cnnclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+
+STAGE_NAME = "Data ingestion stage"
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===========x")
+except Exception as e :
+    logger.exception(e)
+    raise e
--- test.py
+++ test.py
@@ -1,6 +1,56 @@
-def add(a,b):
-    sum = a+b
-    print(f"the result is {sum}")
+# import zipfile
 
-result = add(5,4)
-print(result)
+# def is_zipfile(filepath):
+#     return zipfile.is_zipfile(filepath)
+
+# print(is_zipfile('artifacts\data_ingestion\data.zip'))
+
+# def check_file_header(filepath):
+#     with open(filepath, 'rb') as f:
+#         header = f.read(4)
+#         return header
+
+# header = check_file_header('artifacts\data_ingestion\data.zip')
+# print(f"File header: {header}")
+import os
+import zipfile
+# print(os.getcwd())
+# print(os.path.isfile('artifacts\data_ingestion\data.zip'))
+
+import os
+import zipfile
+import gdown
+from cnnclassifier import logger
+from cnnclassifier.utils.common import get_size
+from cnnclassifier.entity.config_entity import DataIngestionConfig
+from 
+class DataIngestion:
+    def __init__(self,config: DataIngestionConfig):
+        self.config = config
+    
+    def download_file(self) ->str:
+        '''
+        Fetch data from the url
+        '''
+        try: 
+            dataset_url = self.config.source_URL
+            zip_download_dir = self.config.local_data_file
+            os.makedirs("artifacts/data_ingestion",exist_ok = True)
+            if os.path.isfile('artifacts\data_ingestion\data.zip') == False:
+                logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+                file_id = dataset_url.split("/")[-2]
+                prefix = 'https://drive.google.com/uc?/export=download&id='
+                gdown.download(prefix + file_id,zip_download_dir)
+                logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+
+        except Exception as e:
+            raise e
+        
+    def extract_zip_file(self):
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path,exist_ok = True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
+    
+data_ingest = DataIngestion()
+data_ingest.extract_zip_file()
