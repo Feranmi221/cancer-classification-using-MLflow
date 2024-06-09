74473d98c81c4bc4ce097768751029757cc938a7
Feranmi <orekoyaferanmi5@gmail.com>
2024-06-09 11:29:27 +0100

    prepare base model added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from cnnclassifier import logger 
 from cnnclassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
-
+from cnnclassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 STAGE_NAME = "Data ingestion stage"
 
 try:
@@ -11,3 +11,13 @@ try:
 except Exception as e :
     logger.exception(e)
     raise e
+
+STAGE_NAME = "prepare base model"
+try:
+        logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj2 = PrepareBaseModelTrainingPipeline()
+        obj2.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx===========x")
+except Exception as e :
+        logger.exception(e)
+        raise e
---
+++ src/cnnclassifier/components/prepare_base_model.py
@@ -0,0 +1,77 @@
+import os
+import urllib.request as request
+from zipfile import ZipFile
+import tensorflow as tf
+from tensorflow.keras.layers import Conv2D,Dense,MaxPooling2D,Flatten,BatchNormalization,Dropout # type: ignore
+from cnnclassifier.entity.config_entity import PrepareBaseModelConfig
+from pathlib import Path
+
+class PrepareBaseModel:
+    def __init__(self,config: PrepareBaseModelConfig):
+        self.config = config
+    def get_base_model(self):
+        self.model = tf.keras.applications.vgg16.VGG16(
+            input_shape = self.config.params_image_size,
+            weights= self.config.params_weights,
+            include_top= self.config.params_include_top)
+        self.save_model(path=self.config.base_model_path, model=self.model)
+
+    @staticmethod
+    def _prepare_full_model(model,classes,freeze_all,freeze_till,learning_rate):
+        if freeze_all:
+            for layer in model.layers:
+                model.trainable = False
+        elif(freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till > 0]:
+                model.trainable = False
+            
+        flatten_in = Flatten()(model.output)
+        x = BatchNormalization()(flatten_in)
+        x = Dense(1024, activation='relu')(x)
+        x = Dropout(0.3)(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dropout(0.3)(x)
+        x = Dense(256, activation='relu')(x)
+        x = Dense(128, activation='relu')(x)
+        prediction = Dense(units=classes, activation="softmax")(x)
+        
+        full_model = tf.keras.models.Model(
+            inputs = model.input,
+            outputs = prediction
+        )
+        full_model.compile(optimizer = tf.keras.optimizers.Adam(learning_rate = learning_rate),
+                           loss = tf.keras.losses.CategoricalCrossentropy(),
+                           metrics=["accuracy"]
+                           )
+        
+        full_model.summary()
+        return full_model
+    
+    def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model = self.model,
+            classes = self.config.params_classes,
+            freeze_all = True,
+            freeze_till = None,
+            learning_rate = self.config.params_learning_rate
+        )
+        self.save_model(path=self.config.updated_base_model_path,model=self.full_model)
+    
+    @staticmethod
+    def save_model(path:Path,model:tf.keras.Model):
+        model.save(path)
+ 
+    
+    
+    
+
+
+    
+
+    
+
+        
+
+
+
+
